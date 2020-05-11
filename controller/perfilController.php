336c197944ac5c0ea2a194f3c2695076a8a7d0e0
brayanranda <brayanranda@gmail.com>
2020-05-10 22:29:55 -0500

    Vista Perfil

---
+++ controller/perfilController.php
@@ -0,0 +1,10 @@
+<?php
+class PerfilController 
+{
+	function index(){
+		require_once 'views/layout/header.php';
+		require_once 'views/perfil/index.php';
+		require_once 'views/layout/footer.php';
+	}
+}
+?>
---
+++ views/perfil/index.php
@@ -0,0 +1,124 @@
+<section class="perfil">
+    <article class="perfil__content">
+        <div class="perfil__title">
+            <h1 class="perfil__title--h1">Eventos</h1>
+            <p class="perfil__title--txt">
+                Proyectos desarrollados por nuestros miembros.
+            </p>
+        </div>
+        <div class="perfil__grid">
+            <div class="perfil__post">
+                <figure class="perfil__fig">
+                    <img class="perfil__img" src="<?php echo BASE_URL;?>assets/imagen/imgevent.jpg">
+                </figure>
+                <div class="perfil__post__info">
+                    <div class="perfil__post__info__content">
+                        <h2 class="perfil__post__info--title">Curso Nodejs</h2>
+                        <p class="perfil__post__info--date">10/05/2020</p>
+                        <p class="perfil__post__info--txt">
+                            Se utiliza este texto en los diseños 
+                            para que, al estar escrito en un 
+                            lenguaje clásico y no entenderse...
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil__post">
+                <figure class="perfil__fig">
+                    <img class="perfil__img" src="<?php echo BASE_URL;?>assets/imagen/imgevent.jpg">
+                </figure>
+                <div class="perfil__post__info">
+                    <div class="perfil__post__info__content">
+                        <h2 class="perfil__post__info--title">Curso Nodejs</h2>
+                        <p class="perfil__post__info--date">10/05/2020</p>
+                        <p class="perfil__post__info--txt">
+                            Se utiliza este texto en los diseños 
+                            para que, al estar escrito en un 
+                            lenguaje clásico y no entenderse...
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil__post">
+                <figure class="perfil__fig">
+                    <img class="perfil__img" src="<?php echo BASE_URL;?>assets/imagen/imgevent.jpg">
+                </figure>
+                <div class="perfil__post__info">
+                    <div class="perfil__post__info__content">
+                        <h2 class="perfil__post__info--title">Curso Nodejs</h2>
+                        <p class="perfil__post__info--date">10/05/2020</p>
+                        <p class="perfil__post__info--txt">
+                            Se utiliza este texto en los diseños 
+                            para que, al estar escrito en un 
+                            lenguaje clásico y no entenderse...
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil__post">
+                <figure class="perfil__fig">
+                    <img class="perfil__img" src="<?php echo BASE_URL;?>assets/imagen/imgevent.jpg">
+                </figure>
+                <div class="perfil__post__info">
+                    <div class="perfil__post__info__content">
+                        <h2 class="perfil__post__info--title">Curso Nodejs</h2>
+                        <p class="perfil__post__info--date">10/05/2020</p>
+                        <p class="perfil__post__info--txt">
+                            Se utiliza este texto en los diseños 
+                            para que, al estar escrito en un 
+                            lenguaje clásico y no entenderse...
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil__post">
+                <figure class="perfil__fig">
+                    <img class="perfil__img" src="<?php echo BASE_URL;?>assets/imagen/imgevent.jpg">
+                </figure>
+                <div class="perfil__post__info">
+                    <div class="perfil__post__info__content">
+                        <h2 class="perfil__post__info--title">Curso Nodejs</h2>
+                        <p class="perfil__post__info--date">10/05/2020</p>
+                        <p class="perfil__post__info--txt">
+                            Se utiliza este texto en los diseños 
+                            para que, al estar escrito en un 
+                            lenguaje clásico y no entenderse...
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil__post">
+                <figure class="perfil__fig">
+                    <img class="perfil__img" src="<?php echo BASE_URL;?>assets/imagen/imgevent.jpg">
+                </figure>
+                <div class="perfil__post__info">
+                    <div class="perfil__post__info__content">
+                        <h2 class="perfil__post__info--title">Curso Nodejs</h2>
+                        <p class="perfil__post__info--date">10/05/2020</p>
+                        <p class="perfil__post__info--txt">
+                            Se utiliza este texto en los diseños 
+                            para que, al estar escrito en un 
+                            lenguaje clásico y no entenderse...
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="perfil__post">
+                <figure class="perfil__fig">
+                    <img class="perfil__img" src="<?php echo BASE_URL;?>assets/imagen/imgevent.jpg">
+                </figure>
+                <div class="perfil__post__info">
+                    <div class="perfil__post__info__content">
+                        <h2 class="perfil__post__info--title">Curso Nodejs</h2>
+                        <p class="perfil__post__info--date">10/05/2020</p>
+                        <p class="perfil__post__info--txt">
+                            Se utiliza este texto en los diseños 
+                            para que, al estar escrito en un 
+                            lenguaje clásico y no entenderse...
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+</section>
