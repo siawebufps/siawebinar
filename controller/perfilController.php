499d3dd5b01e64e6144c19fc18a2538c0896c4ea
brayanranda <brayanranda@gmail.com>
2020-05-15 21:03:27 -0500

    Ajustes frontend perfil

--- controller/perfilController.php
+++ controller/perfilController.php
@@ -2,7 +2,7 @@
 class PerfilController 
 {
 	function index(){
-		require_once 'views/layout/header.php';
+		require_once 'views/layout/hperfil.php';
 		require_once 'views/perfil/index.php';
 		require_once 'views/layout/footer.php';
 	}
---
+++ views/layout/hperfil.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" type="ico/png" href="<?php echo BASE_URL;?>assets/imagen/favicon.png">
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL;?>assets/scss/style.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL;?>assets/iconos/style.css">
+
+	<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="<?php echo BASE_URL;?>assets/js/main.js"></script>
+	<meta charset="utf-8">
+	<title>Siawebinar</title>
+</head>
+<body>
+
+<header class="header__two">
+	<nav class="header__two__content">
+		<figure class="header__fig">
+			<a href="<?php echo BASE_URL;?>">
+				<img class="header__logo" src="<?php echo BASE_URL;?>assets/imagen/logo.png">
+			</a>
+			<span id="btn-menu" class="header__btn-menu icon-menu"></span>
+		</figure>
+		<ul id="header__menu" class="header__menu">
+			<li class="header__item">
+				<a class="header__two__item--a" href="<?php echo BASE_URL;?>evento/">Eventos</a>
+			</li>
+			<li class="header__item">
+				<a class="header__two__item--a" href="<?php echo BASE_URL;?>semilleros/">Semilleros</a>
+			</li>
+		</ul>
+	</nav>
+	<div class="header__two__slider">
+		<h1 class="header__two__slider--h1">Semillero Siaweb</h1>
+		<p class="header__two__slider--p">
+			Lorem ipsum es, básicamente, un texto
+			de relleno que se utiliza en diseño gráfico 
+		</p>
+	</div>
+	<div class="header__two__video">
+		<video class="header__two__vi" 
+		src="<?php echo BASE_URL;?>assets/video/siaweb.mp4" type="video/ogg" autoplay loop></video>
+	</div>
+</header>
